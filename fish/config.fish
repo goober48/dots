00b304441a07a1f4b37bb2c584b0708e1e3219eb
goober48 <465897@bsd48.org>
2026-03-11 01:12:46 -0700

    this should fix

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,18 @@
-/home/user/.config/fish/config.fish
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+if test -z "$WAYLAND_DISPLAY"; and test "$XDG_VTNR" = 1
+    exec sway
+end
+
+# terminal-wakatime setup
+set -gx PATH "$HOME/.wakatime" $PATH
+terminal-wakatime init fish | source
+
+#maybe this can fix vscode
+set -gx PATH "$HOME/wpilib/2026/frccode" $PATH
+#test
