97555905000098051a3bd731c960ff77346aef52
goober48 <465897@bsd48.org>
2026-04-16 00:23:43 -0700

    audio keybinds finally???

--- dwm/config.h
+++ dwm/config.h
@@ -61,6 +61,9 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *upvol[] = {"amixer", "set", "Master", "5%+", NULL};
+static const char *downvol[] = {"amixer", "set", "Master", "5%-", NULL};
+static const char *togglevol[] = {"amixer", "set", "Master", "toggle", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -97,6 +100,12 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_c,      quit,           {0} },
+    { 0,                            XK_F4,     spawn,          {.v = upvol } },
+	{ 0,                            XK_F3,     spawn,          {.v = downvol } },
+    { 0,                            XK_F2,     spawn,          {.v = togglevol } },
+
+
+
 };
 
 /* button definitions */
--- st/config.h
+++ st/config.h
@@ -122,8 +122,8 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	"#55ffff", /* default foreground colour */
+	"#ffffff", /* default background colour */
 };
 
 
